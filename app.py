55f4cc3766f85fd05c24d40fe9669f2f0df07e4c
saheen-cmd <saheen@getlokalapp.com>
2026-04-02 03:11:45 +0530

    Updated the thread part in code

--- app.py
+++ app.py
@@ -92,7 +92,8 @@ def schedule_feedback(user_id, channel_id):
             app.client.chat_postMessage(
                 channel=channel_id,
                 text="*Please fill the form for any suggestion or issue faced in response: https://forms.gle/gjcFHFs1ubsaqeSv5*",
-                reply_broadcast=True
+                thread_ts=None,
+                reply_broadcast=False
             )
         except Exception as e:
             print(f"Error sending feedback message: {e}")
