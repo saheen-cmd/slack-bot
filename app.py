64838920da719c606dc7af26bd5e5a16221039d5
saheen-cmd <saheen@getlokalapp.com>
2026-04-02 03:39:43 +0530

    updated new code point in feedback

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ def schedule_feedback(user_id, channel_id):
             app.client.chat_postMessage(
                 channel=channel_id,
                 text="*Please fill the form for any suggestion or issue faced in response: https://forms.gle/gjcFHFs1ubsaqeSv5*",
+                as_user=True
             )
         except Exception as e:
             print(f"Error sending feedback message: {e}")
